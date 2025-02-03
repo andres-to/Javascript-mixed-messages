33ca070391b7724a5e1a4520d5b330a874961c13
andres-to <torres-andres@outlook.com>
2025-02-02 21:35:56 -0500

    first commit

---
+++ absurd predictions.js
@@ -0,0 +1,43 @@
+// Mixed messages
+// Predicciones cortas y absurdas con un objeto conteniendo 3 variables (array)
+
+function generatePrediction() {
+    
+    // Objeto con 3 array
+    const data = {
+        object: [
+            "A scientist", "A robot", "An eccentric millionaire", "A hacker", "A time traveler",
+            "An alien", "A cat with artificial intelligence", "A mysterious asteroid",
+            "A failed experiment", "A supercomputer"
+        ],
+        action: [
+            "discovers a secret formula", "activates an interdimensional portal", "loses control of an artificial intelligence",
+            "merges with a machine", "sends a signal to space", "finds a hidden civilization",
+            "invents a new energy source", "unlocks the code of reality", "predicts the end of the world",
+            "hacks the human mind"
+        ],
+        consequence: [
+            "and accidentally resets Earth from scratch.",
+            "and causes humans to speak in binary.",
+            "and discovers that we are part of a simulation.",
+            "and makes cats rule the world.",
+            "and creates a black hole in their laboratory.",
+            "and ends up trapped in the year 3025.",
+            "and turns fish into intelligent beings.",
+            "and causes objects to come to life.",
+            "and sparks a revolution of toasters.",
+            "and awakens an ancient creature."
+        ]
+    };
+
+    // Aleatoriedad de cada elemento del objeto 
+    const object = data.object[Math.floor(Math.random() * data.object.length)];
+    const action = data.action[Math.floor(Math.random() * data.action.length)];
+    const consequence = data.consequence[Math.floor(Math.random() * data.consequence.length)];
+    
+    // Frase generada usando los 3 array del objeto
+    return `${object} ${action} ${consequence}`;
+}
+
+// Ejecutar función que contiene el objeto, a su vez 3 array, y 3 aleatoriedades en cada iteración
+console.log(generatePrediction());
